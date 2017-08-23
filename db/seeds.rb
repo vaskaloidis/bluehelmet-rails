3e89c1f908c90855123ee96f420dd73c159d106c
Vas Kaloidis <vas.kaloidis@gmail.com>
2017-08-23 10:27:41 -0400

    Fixed seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,44 +8,6 @@ User.create!(email: "customer@gmail.com", password: "letmein", role: "customer",
              firstName: Faker::Name.first_name, lastName: Faker::Name.last_name,
              company: Faker::Company.name, phone: Faker::PhoneNumber.phone_number)
 
-User.create!(email: Faker::Internet.email, password: "letmein", role: "customer",
-             firstName: Faker::Name.first_name, lastName: Faker::Name.last_name,
-             company: Faker::Company.name, phone: Faker::PhoneNumber.phone_number)
-User.create!(email: Faker::Internet.email, password: "letmein", role: "customer",
-             firstName: Faker::Name.first_name, lastName: Faker::Name.last_name,
-             company: Faker::Company.name, phone: Faker::PhoneNumber.phone_number)
-User.create!(email: Faker::Internet.email, password: "letmein", role: "customer",
-             firstName: Faker::Name.first_name, lastName: Faker::Name.last_name,
-             company: Faker::Company.name, phone: Faker::PhoneNumber.phone_number)
-User.create!(email: Faker::Internet.email, password: "letmein", role: "customer",
-             firstName: Faker::Name.first_name, lastName: Faker::Name.last_name,
-             company: Faker::Company.name, phone: Faker::PhoneNumber.phone_number)
-User.create!(email: Faker::Internet.email, password: "letmein", role: "customer",
-             firstName: Faker::Name.first_name, lastName: Faker::Name.last_name,
-             company: Faker::Company.name, phone: Faker::PhoneNumber.phone_number)
-User.create!(email: Faker::Internet.email, password: "letmein", role: "customer",
-             firstName: Faker::Name.first_name, lastName: Faker::Name.last_name,
-             company: Faker::Company.name, phone: Faker::PhoneNumber.phone_number)
-
-User.create!(email: Faker::Internet.email, password: "letmein", role: "customer",
-             firstName: Faker::Name.first_name, lastName: Faker::Name.last_name,
-             company: Faker::Company.name, phone: Faker::PhoneNumber.phone_number)
-User.create!(email: Faker::Internet.email, password: "letmein", role: "customer",
-             firstName: Faker::Name.first_name, lastName: Faker::Name.last_name,
-             company: Faker::Company.name, phone: Faker::PhoneNumber.phone_number)
-User.create!(email: Faker::Internet.email, password: "letmein", role: "customer",
-             firstName: Faker::Name.first_name, lastName: Faker::Name.last_name,
-             company: Faker::Company.name, phone: Faker::PhoneNumber.phone_number)
-User.create!(email: Faker::Internet.email, password: "letmein", role: "customer",
-             firstName: Faker::Name.first_name, lastName: Faker::Name.last_name,
-             company: Faker::Company.name, phone: Faker::PhoneNumber.phone_number)
-User.create!(email: Faker::Internet.email, password: "letmein", role: "customer",
-             firstName: Faker::Name.first_name, lastName: Faker::Name.last_name,
-             company: Faker::Company.name, phone: Faker::PhoneNumber.phone_number)
-
-
 
 WorkCategory.create!(name: "Web", htmlId: "php")
 WorkCategory.create!(name: "Informatics Software", htmlId: "informatics_software")
-
-WorkType.create()
